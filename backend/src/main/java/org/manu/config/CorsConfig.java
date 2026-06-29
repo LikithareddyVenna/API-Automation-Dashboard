81546bffc3a297b3e2136b7ebed8d148e092c9ef
MANU <boggarapumanojsai@gmail.com>
2026-06-29 20:54:40 +0530

    solved the cors issue

---
+++ backend/src/main/java/org/manu/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package org.manu.config;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@ConditionalOnProperty(name = "app.cors.enabled", havingValue = "true")
+public class CorsConfig {
+
+    @Value("${app.cors.frontend-url}")
+    private String frontendUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendUrl)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
